4e710ff84285d3229b3d102a5a6f8e3d62caf580
Nils Jarle Haugen <nils@gaupne.net>
2020-02-21 13:17:08 +0100

    implmented avg and mean as asked in 6.16

--- pythonmagicstuff.py
+++ pythonmagicstuff.py
@@ -2,19 +2,54 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("/home/trygve/Documents/github/PredatorPreySimulation/all-survive/01A-hawk-log-2020-02-21T11:01:01.566.csv")
+df = pd.read_csv("./all-survive/01A-hawk-log-2020-02-21T11:01:01.566.csv")
 # /home/trygve/Documents/github/PredatorPreySimulation/Alle_dør .csv
-print(f"Age std: {df['Age'].std()}")
-print(f"Age avg: {df['Age'].mean()}")
+#print(f"Age std: {df['Age'].std()}")
+#print(f"Age avg: {df['Age'].mean()}")
 #hawk_df = df.filter(like='', axis=0)
 
  
-df2 = pd.read_csv("/home/trygve/Documents/github/PredatorPreySimulation/all-survive/01A-population-log-2020-02-21T11:01:01.566.csv")
-df2.cumsum()
+df2 = pd.read_csv("./all-survive2/pop.csv")
+df3 = pd.read_csv("./hawkdies/pop.csv")
+df4 = pd.read_csv("./bothDies/pop.csv")
+
+
+hawkdies_hawk = pd.read_csv("./hawkdies/hawk.csv")
+hawkdies_squirrel = pd.read_csv("./hawkdies/squirrel.csv")
+
+bothDies_hawk = pd.read_csv("./bothDies/hawk.csv")
+bothDies_squirrel = pd.read_csv("./bothDies/squirrel.csv")
+
+alls_hawk = pd.read_csv("./all-survive2/hawk.csv")
+alls_squirrel = pd.read_csv("./all-survive2/squirrel.csv")
+
+
+print("hawk dies mean and avg")
+for x in [hawkdies_hawk,hawkdies_squirrel]:
+    print(f"Age std: {x['Age'].std()}")
+    print(f"Age avg: {x['Age'].mean()}")
+
+print("both dies mean and avg")
+for x in [bothDies_hawk,bothDies_squirrel]:
+    print(f"Age std: {x['Age'].std()}")
+    print(f"Age avg: {x['Age'].mean()}")
+
+print("all survive mean and avg")
+for x in [alls_hawk,alls_squirrel]:
+    print(f"Age std: {x['Age'].std()}")
+    print(f"Age avg: {x['Age'].mean()}")
+
+df3 = pd.read_csv("./hawkdies/pop.csv")
+df4 = pd.read_csv("./bothDies/pop.csv")
+
+#df2.cumsum()
 
 #plt.figure()
-print(df2)
-df2["idx"] = range(len(df2))
-ax=df2.plot("idx", "SQUIRREL")
-df2.plot("idx", "HAWK",secondary_y=True, ax=ax)
-plt.show()
+#print(df2)
+
+for x in [df2, df3, df4]:
+    x["idx"] = range(len(x))
+    ax=x.plot("idx", "SQUIRREL")
+    x.plot("idx", "HAWK",secondary_y=True, ax=ax)
+    #plt.show()
+    
